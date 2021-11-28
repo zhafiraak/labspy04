a7580017b6977545840479390c4d1bbf55c04c82
zhafiraak <zhafirakhafianti19@gmail.com>
2021-11-28 19:06:51 +0700

    file akses_list

---
+++ akses_list.py
@@ -0,0 +1,16 @@
+# Akses List
+## Menampilkan element ke-3
+warna = ['merah', 'kuning', 'hijau', 'biru', 'pink', 'abu']
+print()
+print("List :", warna)
+print()
+print('Warna dalam element ke-3 adalah', warna[3])
+print()
+
+## Ambil nilai element ke-2 sampai ke-4
+print(warna[2:4])
+print()
+
+## Ambil element terakhir
+print(warna[-1])
+print()
