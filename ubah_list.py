123a6ffd3762b78f9fe4cd1128eac8a38798c05e
zhafiraak <zhafirakhafianti19@gmail.com>
2021-11-28 19:08:30 +0700

    file ubah_list

---
+++ ubah_list.py
@@ -0,0 +1,14 @@
+# Ubah element list
+## ubah elemen ke-4 dengan nilai lainnya
+warna = ['merah', 'kuning', 'hijau', 'biru', 'pink', 'abu']
+print()
+print("List sebelum di ubah :", warna)
+print()
+warna[4] = 'ungu'
+print("List sesudah di ubah :", warna)
+print()
+
+## ubah elemen ke 4 sampai dengan elemen terakhir
+warna[4:] = ["hitam", "putih"]
+print("Ubah element ke-4 hingga akhir :", warna)
+print()
